0200efd5fd6cac78c737f31cfb418fe4d608b2da
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-26 05:04:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ class Meal{
 
   deliveries(){
     return store.deliveries.filter(function(delivery){
-      return delivery.mealId = this.id;
+      return delivery.mealId === this.id;
     }.bind(this));
   }
 
