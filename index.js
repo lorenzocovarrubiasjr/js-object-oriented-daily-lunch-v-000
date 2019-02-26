9b2118b6a84e48cd4ea4fa3aeb5e8ca8034da782
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-26 03:58:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,60 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let   neighborhoodId = 0;
+class Neigborhood {
+
+    constructor(name){
+      this.name = name;
+      this.id = ++neighborhoodId;
+      store.neighborhoods.push(this)
+    }
+
+    deliveries(){
+      
+    }
+
+    customers(){
+
+  
+    }
+
+    meals(){
+    
+    }
+}
+
+let customerId = 0;
+class Customer {
+    constructor(name, neighborhoodId){
+      this.name = name,
+      this.neighborhoodId = neighborhoodId;
+      this.id = ++customerId;
+      store.customers.push(this);
+    }
+
+    deliveries(){
+      
+    }
+
+    meals(){
+      
+    }
+
+    totalSpent(){
+      
+    }
+}
+
+let mealId = 0;
+class Meal(){
+  constructor(title, price){
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+    store.meals.push(this);
+  }
+}
+
+
+
