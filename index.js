5d55ad3adfc9ecbfcbef2dadf740bbcbbf1a62e2
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-26 04:26:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ class Neighborhood{
     }
 
     customers(){
-
+      return store.customers.filter(function(customer){
+        return customer.neighborhoodId === this.id;
+      }.bind(this));
 
     }
 
